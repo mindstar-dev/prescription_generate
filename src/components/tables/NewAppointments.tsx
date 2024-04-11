7aa03579f645cd092c43e54d31d7655ccb38a79e
Biswajit9999 <2003biswajit@gmail.com>
2024-04-11 11:09:48 +0530

    Again done

--- src/components/tables/NewAppointments.tsx
+++ src/components/tables/NewAppointments.tsx
@@ -273,9 +273,9 @@ const NewAppointments: React.FunctionComponent = () => {
         <input type="date" className="m-1" />
         <button className="w-[55px] h-[42px] text-white bg-[#F36562] flex justify-center items-center"><FaSearch /></button>
       </div>
-      <div className="h-[75%] w-full overflow-y-scroll">
+      <div className="h-[75%] w-full">
         <div className="w-full h-full overflow-y-scroll flex flex-col">
-          <div className="h-[86px] sticky top-0 bg-white">
+          <div className="h-[86px] sticky top-0 bg-white flex flex-row w-full">
             <div className="text-center w-[15%] h-[86px]">P-ID</div>
             <div className="text-center w-[20%] h-[86px]">NAME</div>
             <div className="text-center w-[15%] h-[86px]">AGE</div>
@@ -287,10 +287,10 @@ const NewAppointments: React.FunctionComponent = () => {
             key={index}
             className={`${index % 2 !== 0 ? "bg-[#F9F1F1]" : "bg-[#F0F0F0]"} flex h-[86px]`}
           >
-            <div className="w-[15%] text-center flex items-center">{item.id}</div>
-            <div className="w-[20%] text-center flex items-center">{item.name}</div>
-            <div className="w-[15%] text-center flex items-center">{item.age}</div>
-            <div className="w-[20%] text-center flex items-center">{item.phno}</div>
+            <div className="w-[15%]  flex items-center justify-center">{item.id}</div>
+            <div className="w-[20%]  flex items-center justify-center">{item.name}</div>
+            <div className="w-[15%] flex items-center justify-center">{item.age}</div>
+            <div className="w-[20%]  flex items-center justify-center">{item.phno}</div>
             <div className="w-[30%] space-x-4 flex items-center">
               <button className="h-[41px] w-[95px] bg-[#FCA19F] hover:bg-[#F36562]">
                 DETAILS
